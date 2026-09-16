8a1199e54dfbed33015adf20b5bfdc41c9f88d00
Deepak Vijayagopalan <deepak.ramajayam@stu.ocsb.ca>
2026-09-16 13:33:27 +0000

    Added basic_math.py

---
+++ .github/basic_math.py
@@ -0,0 +1,19 @@
+#!/usr/bin/envpython3
+# Created By:Deepak
+# Date: 06-06-2010
+# This program does basic math.
+
+import math
+
+
+def main():
+    print("6 + 6={}".format(6 + 6))
+    print("53 - 28={}".format(53 - 28))
+    print("6 * 6={}".format(6 * 6))
+    print("9 / 3={}".format(9 / 3))
+    print("3 ^ 5={}".format(3 ^ 5))
+    print("√8={}".format(math.sqrt(8)))
+
+
+if __name__ == "__main__":
+    main()
